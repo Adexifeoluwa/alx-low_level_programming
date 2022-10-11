720bbf996ffa4b6d58230be07b128f2cebe6493b
Adexifeoluwa <105643190+Adexifeoluwa@users.noreply.github.com>
2022-10-11 02:17:52 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - structure defination for a dog
+ *
+ * @name: Character array.
+ *
+ * @age: float.
+ *
+ * @owner: character array.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
