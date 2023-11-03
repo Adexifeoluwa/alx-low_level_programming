9e30384a14f60a1aade051f5de53418137ca2ffc
Adexifeoluwa <adeniranifeoluwa99@gmail.com>
2023-11-02 22:39:20 -0700

    Functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+* Check if it is UpperCase or Not
+*check using ASCII Value
+*ASCII Value for (A - Z) 65 - 90)
+*covert every charater givern to its ASCII Value
+*compare tge ACSII Value if it falls within the range
+*if it does, then return 1, otherwise return 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && is c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
