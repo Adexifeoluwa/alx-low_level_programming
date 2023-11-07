6905360db1f7a594b64c740ca32e387dc65e5245
Adexifeoluwa <adeniranifeoluwa99@gmail.com>
2023-11-07 15:53:54 -0800

    first struct in ALX

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+#include<stdio.h>
+
+/**
+ * struct dog - a struct that create information for a dog
+ * @name: This is the name of the dog
+ * @age: This is the dog age
+ * @owner: This is the owner of the dog
+ * Description: first struct with ALX
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
