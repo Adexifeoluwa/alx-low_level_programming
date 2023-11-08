c93372ceeca6c620245bcd99b8c8d57119365ade
Adexifeoluwa <adeniranifeoluwa99@gmail.com>
2023-11-07 16:45:09 -0800

    struct 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ *init_dog - this is the new dog struct whosr variable is to be initialized
+ *@d: The pointer to the struct of the new dog
+ *@name: name of the neww dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	//d = malloc(sizeof(struct dog)); //Memory allocation gone wrong 
+	if (d == NULL)
+		return;
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
